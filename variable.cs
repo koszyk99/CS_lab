f4f94ef1b3835f087d36b87253b40f0c527a0647
koszyk99 <krzysztof_koszyk@interia.eu>
2026-06-29 18:54:15 +0200

    feat: create variable.cs

---
+++ variable.cs
@@ -0,0 +1,16 @@
+// syntax: type name = value 
+
+int age = 27;
+string name = "Krzysztof";
+double temperature = 36.6;
+bool is_true = true;
+
+// you can declare without assignment - but it must be done before use
+int number;
+number = 27;
+
+// var - the compiler infers the type itself
+var country = "Poland";
+var year = 2026;
+
+Console.WriteLine($"Hi, {name}! You have {age} years old.");
