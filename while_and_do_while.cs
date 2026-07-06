ee348bcf25341732d2453547b2d93f9646005437
koszyk99 <krzysztof_koszyk@interia.eu>
2026-07-06 20:42:03 +0200

    feat: update while and do while.cs

--- while_and_do_while.cs
+++ while_and_do_while.cs
@@ -15,3 +15,11 @@ do
     Console.WriteLine($"n = {n}");
     n--;
 } while (n > 8);
+
+// break and continue
+for (int i = 0; i < 10; i++)
+{
+    if (i == 3) continue; // skip 3
+    if (i == 7) break; // stop at 7
+    Console.Write($"{i} ");
+}
