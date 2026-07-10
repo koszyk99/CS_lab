8a52ef05c7cb871fc97db687d0357082519733da
koszyk99 <krzysztof_koszyk@interia.eu>
2026-07-10 20:43:25 +0200

    fix: update overloading.cs

--- overloading.cs
+++ overloading.cs
@@ -1,15 +1,15 @@
 using System;
 
-// compiler choose right version
-static double CalculateArea(double side) // square
-    => side * side;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(CalculateArea(5));
+        Console.WriteLine(CalculateArea(4, 6));
+        Console.WriteLine(CalculateArea(3, true));
+    }
 
-static double CalculateArea(double width, double height) // rectangle
-    => width * height;
-
-static double CalculateArea(double r, bool circle) // circle
-    => Math.PI * r * r;
-
-Console.WriteLine(CalculateArea(5));
-Console.WriteLine(CalculateArea(4, 6));
-Console.WriteLine(CalculateArea(3, true));
+    static double CalculateArea(double side) => side * side;
+    static double CalculateArea(double width, double height) => width * height;
+    static double CalculateArea(double r, bool circle) => Math.PI * r * r;
+}
